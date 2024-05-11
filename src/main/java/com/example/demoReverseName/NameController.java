28c0c8b0485ebd896a31ea0c853e775370549ed6
Sergio <sergio.hanganu@gmail.com>
2024-05-11 18:58:30 +0200

    PostMapping done

--- src/main/java/com/example/demoReverseName/NameController.java
+++ src/main/java/com/example/demoReverseName/NameController.java
@@ -1,6 +1,7 @@
 package com.example.demoReverseName;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class NameController {
     @GetMapping("/name")
     public String getName(@RequestParam String name){
-        return new StringBuilder(name).reverse().toString();
+        return name;
+    }
+    @PostMapping("/name")
+    public String getRevertName(@RequestParam String name){
+       return new StringBuilder(name).reverse().toString();
     }
 }
